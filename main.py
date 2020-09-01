b52b43180514f4128254056aebc96fd5b3e62242
vadim <feofanov@yandexlyceum.ru>
2020-09-01 09:20:04 +0500

    Rebasing

--- main.py
+++ main.py
@@ -2,78 +2,58 @@
 import requests
 from tqdm import *
 import csv
+import json
+
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0',
            'Accept': 'application/json, text/javascript, */*; q=0.01'}
 
-start_url = 'https://russiaedu.ru/_ajax/schools?edu_school_filter%5BschoolName%5D=&edu_school_filter%5Bregion%5D=' \
-      '&edu_school_filter%5Bdistrict%5D=&edu_school_filter%5BformType%5D=&edu_school_filter%5BownershipType%5D=' \
-      '&edu_school_filter%5B_token%5D=mZ4TgkB-uxCGEws3rnAwECJLcbCLooAiS2kGOogr0cA&pp=1&pageNumber=1&direction='
-
-json_ = requests.get(start_url, headers=headers)
-pages = int(json_.json()['pageCount'])
-base = [['Название школы', 'Директор', 'Адрес', 'Тип населенного пункта', 'Район', 'Регион', 'Телефон', 'Сайт', 'Email', 'Источник']]
-
-
-def writing_raw(data):
-    current = []
-    current.append(data['data']['title'])
-    current.append(data['data']['director'])
-    current.append(data['data']['address'].strip())
-    if 'г.' in data['data']['address'] or 'город' in data['data']['address']:
-        current.append('Город')
-    elif 'п.' in data['data']['address']:
-        current.append('Посёлок')
-    elif 'с.' in data['data']['address'] or 'село' in data['data']['address']:
-        current.append('Село')
-    elif 'п.г.т.' in data['data']['address'] or 'поселок городского типа' in data['data']['address']:
-        current.append('Посёлок городского типа')
-    elif 'д.' in data['data']['address'] or 'деревня' in data['data']['address']:
-        current.append('Деревня')
-    elif 'р.п.' in data['data']['address'] or 'рабочий посёлок' in data['data']['address']:
-        current.append('Рабочий посёлок')
-    else:
-        current.append('Город')
-    if 'район' in data['data']['address']:
-        for i in data['data']['address'].split(','):
-            if 'район' in i:
-                current.append(i.strip())
-    else:
-        current.append(None)
-    if 'область' in data['data']['address'] or 'обл' in data['data']['address'] or 'об' in data['data']['address']:
-        for i in data['data']['address'].split(','):
-            if 'область' in i or 'обл' in i or 'об' in i:
-                current.append(i.strip())
-    elif 'край' in data['data']['address']:
-        for i in data['data']['address'].split(','):
-            if 'край' in i:
-                current.append(i.strip())
-    elif 'Республика' in data['data']['address']:
-        for i in data['data']['address'].split(','):
-            if 'Республика' in i:
-                current.append(i.strip())
-    else:
-        current.append(None)
-    current.append(data['data']['phone'])
-    current.append(data['data']['site'])
-    current.append(data['data']['email'])
-    current.append(f"https://russiaedu.ru{data['link']}")
-    return current
+start_url = 'https://bus.gov.ru/public-rest/api/agency/search/init?d-442831-p=1&orderAttributeName=rank&' \
+            'orderDirectionASC=false&pageSize=30&searchString=школа&searchTermCondition=or'
+search_page = requests.get(start_url, headers=headers)
+base = [['Название школы', 'Директор', 'Адрес', 'Тип населенного пункта', 'Регион', 'Телефон', 'Сайт', 'Email', 'Источник']]
+current_pages = search_page.json()['agencies']
+with open('regions.json', 'r', encoding='utf-8') as fh:
+    data = json.load(fh)
 
 
-print('Staring parse...')
-with tqdm(total=pages // 10000 + 1) as pbar:
-    for i in range(1, pages // 10000 + 2):
-        url = f'https://russiaedu.ru/_ajax/schools?edu_school_filter%5BschoolName%5D=&edu_school_filter%5Bregion%' \
-              f'5D=&edu_school_filter%5Bdistrict%5D=&edu_school_filter%5BformType%5D=&edu_school_filter%' \
-              f'5BownershipType%5D=&edu_school_filter%5B_token%5D=mZ4TgkB-uxCGEws3rnAwECJLcbCLooAiS2kGOogr0cA&pp=' \
-              f'10000&pageNumber={i}&direction='
-        json_to_parse = requests.get(url, headers=headers)
-        for data in json_to_parse.json()['eduSchools']:
-            base.append(writing_raw(data))
-        pbar.update()
-print('Done!')
-with open('base.csv', 'w') as f:
-    w = csv.writer(f)
-    w.writerows(base)
-    f.close()
+def writing_raw(current_pages, region, id):
+    for page in current_pages:
+        page_url = f'https://bus.gov.ru/public/agency/agency.json?agency={id}'
+        json_ = requests.get(page_url, headers=headers).json()
+        name = page['shortName'].strip()
+        address = page['fullAddress'].strip()
+        loc_type = f"{json_['publicInfo']['ppo']['settlementType']}"
+        region = region
+        phone = page['phone'].strip()
+        site = page['website'].strip()
+        link = f"https://bus.gov.ru/pub/info-card/{str(page['agencyId'])}".strip()
+        director = f"{json_['agency']['managerSecondName']} " \
+                   f"{json_['agency']['managerFirstName']} {json_['agency']['managerMiddleName']}"
+        email = f"{json_['agency']['email']}"
+        return [name, director, address, loc_type, region, phone, site, email, link]
+
+
+if __name__ == '__main__':
+    print('Staring parse...')
+    with tqdm(total=len(data)) as pbar:
+        for reg, id in data.items():
+            print(f'Working at {reg}...')
+            for i in range(1, (int(id[1]) + 1) // 30 + 1):
+                url = f'https://bus.gov.ru/public-rest/api/agency/search/init?d-442831-p={i}&orderAttributeName=rank&' \
+                      f'orderDirectionASC=false&pageSize=30&regionId={str(id[0])}&regions={str(id[0])}&' \
+                      f'searchString=школа&searchTermCondition=or'
+                try:
+                    json_to_parse = requests.get(url, headers=headers)
+                    base.append(writing_raw(json_to_parse.json()['agencies'], reg, str(id[0])))
+                except:
+                    continue
+            pbar.update()
+            print()
+
+    with open('base.csv', 'w') as f:
+        w = csv.writer(f)
+        w.writerows(base)
+        f.close()
+
+    print('Done!')
